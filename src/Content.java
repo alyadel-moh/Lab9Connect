d0b3d7816fb0983cc3f542fd51c285c24754cd38
menna812 <140957372+menna812@users.noreply.github.com>
2024-12-02 02:22:53 +0200

    Update Content.java

--- src/Content.java
+++ src/Content.java
@@ -3,6 +3,35 @@ import java.util.Date;
 public abstract class Content {
     private String contentId;
     private String authorId;
-    private String Content;
+    private String content;
     private Date timeStamp;
+
+    public String getContentId(){
+        return contentId;
+    }
+    public String getAuthorId(){
+        return authorId;
+    }
+    public String getContent(){
+        return content;
+    }
+    public Date getDate(){
+        return timeStamp;
+    }
+
+    public void setContentId(String contentId) {
+        this.contentId = contentId;
+    }
+
+    public void setAuthorId(String authorId) {
+        this.authorId = authorId;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setTimeStamp(Date timeStamp) {
+        this.timeStamp = timeStamp;
+    }
 }
