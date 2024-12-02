59f37bc52368c6dd739cfe0b5f449dac0e8240fc
menna812 <140957372+menna812@users.noreply.github.com>
2024-12-02 14:43:51 +0200

    Update Posts.java

--- src/java/coding/Posts.java
+++ src/java/coding/Posts.java
@@ -1,6 +1,12 @@
 package coding;
 
+import java.time.LocalDateTime;
+
 public class Posts extends Content{
+
+    Posts(String contentId, String authorId, String content, LocalDateTime timeStamp){
+        super(contentId,authorId,content,timeStamp);
+    }
 
     @Override
     public boolean isExpired(){
