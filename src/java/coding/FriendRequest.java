8788e40c9e0487f4f6e1452fb200980bc8c5ac06
Menna <mennahassan722005@gmail.com>
2024-12-12 22:45:36 +0200

    loadingRequestsWorkingAfterChangingItToGenerics

--- src/java/coding/FriendRequest.java
+++ src/java/coding/FriendRequest.java
@@ -1,7 +1,10 @@
 package coding;
 
-public class FriendRequest extends Request{
-    public FriendRequest(){
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+public class FriendRequest extends Request {
+    public FriendRequest() {
         super();
     }
 
@@ -9,8 +12,22 @@ public class FriendRequest extends Request{
         super(sender, receiver);
     }
 
-    public User getReceiver(){
-        return (User) this.receiver;
-    }
 
+    /// ////////////////////////////////////////////////////
+    public User getReceiver() {
+//        System.out.println(this.receiver);
+//        System.out.println(receiver.getClass().getName());
+        //since it is generics first time it will be loaded as a linked hashmap then here we change it to user
+        if (this.receiver.getClass().getName().equals("java.util.LinkedHashMap")) {
+            ArrayList<User> users = Database.getUsers();
+            //searching for the user with the same id to link it with the hashmap loaded from the file
+            for (User user : users) {
+                if (user.getUserId().equals(((LinkedHashMap) receiver).get("userId"))) {
+                    this.receiver = user;
+                    return user;
+                }
+            }
+        }
+            return (User) this.receiver;
+    }
 }
