8aabf21932d6fab632f5ffd0b05a0a4af0cf834e
shamsquraytum <154440456+shamsquraytum@users.noreply.github.com>
2024-12-12 14:42:21 +0200

    observer

--- src/java/coding/Observer/Group_Observer.java
+++ src/java/coding/Observer/Group_Observer.java
@@ -3,6 +3,7 @@ package coding.Observer;
 import coding.CustomPanel;
 import coding.ENUMS.Mapper;
 import coding.User;
+<<<<<<< Updated upstream
 
 public class Group_Observer implements NotificationObserver {
     private final User user;
@@ -17,3 +18,18 @@ public class Group_Observer implements NotificationObserver {
         this.user.getObserver().update(user, CODE);
     }
 }
+=======
+//
+//public class Group_Observer implements NotificationObserver {
+//    private final User user;
+//
+//    public Group_Observer(User user) {
+//        this.user = user;
+//    }
+//
+//    @Override
+//    public void update(User user, String text) {
+//        CustomPanel
+//    }
+//}
+>>>>>>> Stashed changes
--- src/java/coding/Observer/Request_Observer.java
+++ src/java/coding/Observer/Request_Observer.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package coding.Observer;
 
 import coding.CustomPanel;
@@ -19,3 +20,23 @@ public class Request_Observer implements NotificationObserver {
         this.user.getObserver().update(user, CODE);
     }
 }
+=======
+//package coding.Observer;
+//
+//import coding.CustomPanel;
+//import coding.User;
+//
+//public class Request_Observer implements NotificationObserver {
+//    private final User user;
+//
+//    public Request_Observer(User user) {
+//        this.user = user;
+//    }
+//
+//    @Override
+//    public void update(User user, String text) {
+//        CustomPanel customPanel =  new CustomPanel<>(user, "Accept" ,"Request");
+//        customPanel.get
+//    }
+//}
+>>>>>>> Stashed changes
